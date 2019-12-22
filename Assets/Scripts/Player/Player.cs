0ef9baf717ef0ddd2d6397025150534ed99c8a29
NiJeTi <nijeti.inc@gmail.com>
2019-12-22 18:43:23 +0300

    Basic network system with game settings and random-based role assigment

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,41 @@
+﻿using Assets.Scripts.Managing.Game;
+using Assets.Scripts.PlayerScripts.PlayerRoles;
+
+using Mirror;
+
+using UnityEngine;
+
+namespace Assets.Scripts.PlayerScripts
+{
+    public class Player : NetworkBehaviour
+    {
+        public PlayerRole playerRole = new PlayerRole();
+
+        [SerializeField] private Behaviour[] playerTools;
+
+        public void Setup()
+        {
+            CmdSetup();
+        }
+
+        [Command]
+        private void CmdSetup() =>  RpcSetup();
+
+        [ClientRpc]
+        private void RpcSetup() => SetDefaults();
+
+        private void SetDefaults()
+        {
+            Utility.ToggleComponents(ref playerTools, false);
+
+            GameManager.OnGameStart += OnGameStart;
+        }
+
+        private void OnGameStart()
+        {
+            var roleSet = playerRole.GetRoleSet();
+
+            Utility.ToggleComponents(ref roleSet, true);
+        }
+    }
+}
