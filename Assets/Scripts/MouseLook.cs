c28370c994373558ed5c40a7ee513edc0070ef44
Gubanov Ilya <yoyrandao@gmail.com>
2019-12-22 18:59:19 +0300

    [feat]: added player and camera controller scripts

---
+++ Assets/Scripts/MouseLook.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+	[SerializeField] private Transform _playerTransform;
+
+	private float _xRotation;
+
+	[Header("Sensitivity")] [SerializeField] [Range(10.0f, 200.0f)] private float _sensitivity = 100f;
+
+	private void Start()
+	{
+		Cursor.lockState = CursorLockMode.Locked;
+	}
+
+	private void Update()
+	{
+		var mouseX = Input.GetAxis("Mouse X") * _sensitivity * Time.deltaTime;
+		var mouseY = Input.GetAxis("Mouse Y") * _sensitivity * Time.deltaTime;
+
+		_xRotation -= mouseY;
+		_xRotation = Mathf.Clamp(_xRotation, -90f, 90f);
+
+		transform.localRotation = Quaternion.Euler(_xRotation, 0, 0);
+		_playerTransform.Rotate(Vector3.up * mouseX);
+	}
+}
