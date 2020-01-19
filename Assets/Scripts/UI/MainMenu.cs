6f16df7bd8e4dd5f4ec570068d442e5db737e3d1
Gubanov Ilya <yoyrandao@gmail.com>
2020-01-19 15:03:18 +0300

    [feat]: improved settings ui and functionality

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -1,4 +1,10 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using System.Linq;
+
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.EventSystems;
+using UnityEngine.Serialization;
 using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
@@ -8,26 +14,72 @@ public class MainMenu : MonoBehaviour
 
 	[SerializeField] private GameObject settingsUI;
 
+	[FormerlySerializedAs("_buttonAudioSource")]
 	[Header("Audio")]
-	[SerializeField] private AudioSource _buttonAudioSource;
+	[SerializeField] private AudioSource _menuAudioSource;
 
-	[SerializeField] private AudioClip _buttonSound;
+	[SerializeField] private AudioMixer _audioMixer;
 
 	[Header("UI Components")]
 	[SerializeField] private Button[] buttons;
+	[SerializeField] private AudioClip _buttonSound;
+
+	[Space]
+	[SerializeField] private Dropdown resolutionDropdown;
+	[SerializeField] private AudioClip dropdownSelectSound;
+
+	[Space]
+	[SerializeField] private Slider volumeSlider;
+	[SerializeField] private AudioClip sliderChangeSound;
+
+	[Space]
+	[SerializeField] private Toggle fullscreenToggle;
+	[SerializeField] private AudioClip toggleSound;
 
 	private bool isTuning;
+
+	private List<Resolution> resolutions;
 
 	// Start is called before the first frame update
 	private void Start()
 	{
-		_buttonAudioSource.clip = _buttonSound;
+		resolutions = Screen.resolutions.ToList();
+
+		resolutionDropdown.ClearOptions();
+		resolutionDropdown.AddOptions(resolutions.Select(res => res.width + " x " + res.height).ToList());
+
+		resolutionDropdown.value = resolutions.IndexOf(Screen.currentResolution);
+		resolutionDropdown.RefreshShownValue();
 
 		for (int i = 0; i < buttons.Length; i++)
 		{
-			buttons[i].onClick.AddListener(() => _buttonAudioSource.PlayOneShot(_buttonSound));
+			buttons[i].onClick.AddListener(() => _menuAudioSource.PlayOneShot(_buttonSound));
 		}
+
+		resolutionDropdown.onValueChanged.AddListener(_ => _menuAudioSource.PlayOneShot(dropdownSelectSound));
+		volumeSlider.onValueChanged.AddListener(_ => _menuAudioSource.PlayOneShot(sliderChangeSound));
+		fullscreenToggle.onValueChanged.AddListener(_ => _menuAudioSource.PlayOneShot(toggleSound));
 	}
+
+	#region Settings interaction
+
+	public void SetVolume(float volume)
+	{
+		_audioMixer.SetFloat("MasterVolume", volume);
+	}
+
+	public void SetFullscreen(bool isFullscreen)
+	{
+		Screen.fullScreen = isFullscreen;
+	}
+
+	public void SetResolution(int resolutionIndex)
+	{
+		var selectedResolution = resolutions[resolutionIndex];
+		Screen.SetResolution(selectedResolution.width, selectedResolution.height, Screen.fullScreen);
+	}
+
+	#endregion
 
 	public void StartGame()
 	{
